bc88f761f6beedb064bfc0b2f93d3665fcd42bc2
Michael Delva <michael@emidee.net>
2024-06-06 21:37:50 +0200

    Fixed formatting to comply to UE standards (#3)
    Co-authored-by: Michael Delva <michael.delva@fishingcactus.com>

--- Source/Private/Messaging/CommonMessagingSubsystem.cpp
+++ Source/Private/Messaging/CommonMessagingSubsystem.cpp
@@ -1,41 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "Messaging/CommonMessagingSubsystem.h"
 
-#include <Engine/GameInstance.h>
-#include <Engine/LocalPlayer.h>
-#include <UObject/UObjectHash.h>
+#include "Engine/GameInstance.h"
+#include "Engine/LocalPlayer.h"
+#include "UObject/UObjectHash.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(CommonMessagingSubsystem)
 
 class FSubsystemCollectionBase;
 class UClass;
-class UCommonGameDialog;
 
-void UCommonMessagingSubsystem::Initialize( FSubsystemCollectionBase & Collection )
+void UCommonMessagingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-    Super::Initialize( Collection );
+	Super::Initialize(Collection);
 }
 
 void UCommonMessagingSubsystem::Deinitialize()
 {
-    Super::Deinitialize();
+	Super::Deinitialize();
 }
 
-bool UCommonMessagingSubsystem::ShouldCreateSubsystem( UObject * Outer ) const
+bool UCommonMessagingSubsystem::ShouldCreateSubsystem(UObject* Outer) const
 {
-    if ( !CastChecked< ULocalPlayer >( Outer )->GetGameInstance()->IsDedicatedServerInstance() )
-    {
-        TArray< UClass * > ChildClasses;
-        GetDerivedClasses( GetClass(), ChildClasses, false );
+	if (!CastChecked<ULocalPlayer>(Outer)->GetGameInstance()->IsDedicatedServerInstance())
+	{
+		TArray<UClass*> ChildClasses;
+		GetDerivedClasses(GetClass(), ChildClasses, false);
 
-        // Only create an instance if there is no override implementation defined elsewhere
-        return ChildClasses.Num() == 0;
-    }
+		// Only create an instance if there is no override implementation defined elsewhere
+		return ChildClasses.Num() == 0;
+	}
 
-    return false;
+	return false;
 }
 
-void UCommonMessagingSubsystem::ShowConfirmation( UCommonGameDialogDescriptor * dialog_descriptor, TSubclassOf< UCommonGameDialog > custom_dialog_widget, FCommonMessagingResultDelegate result_callback )
+void UCommonMessagingSubsystem::ShowConfirmation(UCommonGameDialogDescriptor* DialogDescriptor, FCommonMessagingResultDelegate ResultCallback)
 {
+	
 }
 
-void UCommonMessagingSubsystem::ShowError( UCommonGameDialogDescriptor * DialogDescriptor, FCommonMessagingResultDelegate ResultCallback )
+void UCommonMessagingSubsystem::ShowError(UCommonGameDialogDescriptor * DialogDescriptor, FCommonMessagingResultDelegate ResultCallback)
 {
+	
+}
+
+void UCommonMessagingSubsystem::ShowConfirmation(UCommonGameDialogDescriptor * DialogDescriptor, TSubclassOf< UCommonGameDialog > CustomDialogWidget, FCommonMessagingResultDelegate ResultCallback)
+{
+	
 }
--- Source/Public/Actions/AsyncAction_ShowConfirmation.h
+++ Source/Public/Actions/AsyncAction_ShowConfirmation.h
@@ -1,85 +1,80 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #pragma once
 
-#include <Kismet/BlueprintAsyncActionBase.h>
-#include <UObject/ObjectPtr.h>
+#include "Kismet/BlueprintAsyncActionBase.h"
 
+#include "UObject/ObjectPtr.h"
 #include "AsyncAction_ShowConfirmation.generated.h"
 
+class UCommonGameDialog;
 enum class ECommonMessagingResult : uint8;
 
 class FText;
-class ULocalPlayer;
-class UCommonGameDialog;
 class UCommonGameDialogDescriptor;
+class ULocalPlayer;
 struct FFrame;
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FCommonMessagingResultMCDelegate, ECommonMessagingResult, Result );
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCommonMessagingResultMCDelegate, ECommonMessagingResult, Result);
 
 /**
- * Allows easily triggering an async confirmation dialog in blueprints that you can then wait on the result.
+* Allows easily triggering an async confirmation dialog in blueprints that you can then wait on the result.
  */
 UCLASS()
-class COMMONGAME_API UAsyncAction_ShowConfirmation : public UBlueprintAsyncActionBase
+class UAsyncAction_ShowConfirmation : public UBlueprintAsyncActionBase
 {
-    GENERATED_UCLASS_BODY()
+	GENERATED_UCLASS_BODY()
 
 public:
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationYesNo(
-        UObject * in_world_context_object,
-        FText title,
-        FText message );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject"))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationYesNo(
+		UObject* InWorldContextObject, FText Title, FText Message
+	);
 
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationOkCancel(
-        UObject * in_world_context_object,
-        FText title,
-        FText message );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject"))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationOkCancel(
+		UObject* InWorldContextObject, FText Title, FText Message
+	);
 
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationCustom(
-        UObject * in_world_context_object,
-        UCommonGameDialogDescriptor * descriptor );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject"))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationCustom(
+		UObject* InWorldContextObject, UCommonGameDialogDescriptor* Descriptor
+	);
 
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationWithCustomWidgetYesNo(
-        UObject * in_world_context_object,
-        FText title,
-        FText message,
-        TSubclassOf< UCommonGameDialog > custom_dialog_widget );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject" ))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationWithCustomWidgetYesNo(
+		UObject* InWorldContextObject, FText Title, FText Message, TSubclassOf<UCommonGameDialog> CustomDialogWidget 
+	);
 
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationWithCustomWidgetOkCancel(
-        UObject * in_world_context_object,
-        FText title,
-        FText message,
-        TSubclassOf< UCommonGameDialog > custom_dialog_widget );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject"))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationWithCustomWidgetOkCancel(
+		UObject* InWorldContextObject, FText Title, FText Message, TSubclassOf<UCommonGameDialog> CustomDialogWidget 
+	);
 
-    UFUNCTION( BlueprintCallable, BlueprintCosmetic, meta = ( BlueprintInternalUseOnly = "true", WorldContext = "in_world_context_object" ) )
-    static UAsyncAction_ShowConfirmation * ShowConfirmationWithCustomWidgetCustom(
-        UObject * in_world_context_object,
-        UCommonGameDialogDescriptor * descriptor,
-        TSubclassOf< UCommonGameDialog > custom_dialog_widget );
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, meta = (BlueprintInternalUseOnly = "true", WorldContext = "InWorldContextObject"))
+	static UAsyncAction_ShowConfirmation* ShowConfirmationWithCustomWidgetCustom(
+		UObject* InWorldContextObject, UCommonGameDialogDescriptor* descriptor, TSubclassOf<UCommonGameDialog> CustomDialogWidget
+	);
 
-    virtual void Activate() override;
+	virtual void Activate() override;
 
 public:
-    UPROPERTY( BlueprintAssignable )
-    FCommonMessagingResultMCDelegate OnResult;
+	UPROPERTY(BlueprintAssignable)
+	FCommonMessagingResultMCDelegate OnResult;
 
 private:
-    void HandleConfirmationResult( ECommonMessagingResult confirmation_result );
-    static UAsyncAction_ShowConfirmation * CreateAction( UObject * in_world_context, UCommonGameDialogDescriptor * descriptor, TSubclassOf< UCommonGameDialog > custom_dialog_widget = nullptr );
+	void HandleConfirmationResult(ECommonMessagingResult ConfirmationResult);
+	static UAsyncAction_ShowConfirmation* CreateAction(UObject* InWorldContext, UCommonGameDialogDescriptor* Descriptor, TSubclassOf<UCommonGameDialog> CustomDialogWidget = nullptr);
 
-    UPROPERTY( Transient )
-    TObjectPtr< UObject > WorldContextObject;
+	UPROPERTY(Transient)
+	TObjectPtr<UObject> WorldContextObject;
 
-    UPROPERTY( Transient )
-    TObjectPtr< ULocalPlayer > TargetLocalPlayer;
+	UPROPERTY(Transient)
+	TObjectPtr<ULocalPlayer> TargetLocalPlayer;
 
-    UPROPERTY( Transient )
-    TObjectPtr< UCommonGameDialogDescriptor > Descriptor;
+	UPROPERTY(Transient)
+	TObjectPtr<UCommonGameDialogDescriptor> Descriptor;
 
-    UPROPERTY( Transient )
-    TSubclassOf< UCommonGameDialog > CustomDialogWidget;
+	UPROPERTY( Transient )
+	TSubclassOf< UCommonGameDialog > CustomDialogWidget;
 };
